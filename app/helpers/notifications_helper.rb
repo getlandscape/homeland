d7e96e563a16f4603b541213c5b1fc561cd4a743
Andy Gao <yidiangao23@gmail.com>
2022-02-21 01:55:26 +0800

    [andy] add group user notification service

---
+++ app/helpers/notifications_helper.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "digest/md5"
+module NotificationsHelper
+  def second_target_tag(notification, opts = {})
+    obj = notification.second_target
+    return t("notifications.group_was_deleted") if obj.blank?
+
+    link_to(obj.name, main_app.group_path(obj), title: obj.name, class: "topic-title")
+  end
+
+  def actor_tag(notification, opts = {})
+    obj = notification.actor
+    return t("notifications.user_was_deleted") if obj.blank?
+
+    link_to(obj.name, main_app.user_path(obj), title: obj.name, class: "topic-title")
+  end
+
+  def receiver_tag(notification, opts = {})
+    obj = notification.user
+    return t("notifications.user_was_deleted") if obj.blank?
+
+    link_to(obj.name, main_app.user_path(obj), title: obj.name, class: "topic-title")
+  end
+end
