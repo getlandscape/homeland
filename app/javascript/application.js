0f7ffd54a68f88c7060b84fc637fb12f2308963b
申思维 <shensiwei@sina.com>
2022-03-19 11:43:56 +0800

    add metamask and polka images

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 import "homeland/index";
 import "vendor/bootstrap";
 import "front/index";
+
+const importAll = (r) => r.keys().map(r)
+importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
